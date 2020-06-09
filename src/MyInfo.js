3b725b53ffa8dfcd9c696c95fa482dfffea611e4
Balwinder Rajput <er.vicky1989@gmail.com>
2020-06-09 14:34:08 +0000

    Initial commit

---
+++ src/MyInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MyInfo()
+{
+return (
+  <div>
+  <h1> THis is heading </h1>
+  <ui> 
+<li>qq</li>
+<li>qq</li>
+<li>qq</li>
+</ui>
+</div>
+
+)
+
+}
+
+export default MyInfo
---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import React from 'react'
+function Header()
+{
+return <header className="navbar">This is Heading </header>
+}
+export default Header
